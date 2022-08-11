0ef1205330511a6aba665a25a222072f7f0b5053
Brandon Wyman <bjwyman@gmail.com>
2022-08-11 23:10:57 +0000

    Skip all the fancy vectory to array attempts.
    Just assume first two are what I want, hardcode a bit?
    
    Change-Id: I607ebd7de5e170fdf7d3cbad8c23adf174d71f76

--- redfish-core/lib/power_supply_metrics.hpp
+++ redfish-core/lib/power_supply_metrics.hpp
@@ -217,13 +217,8 @@ inline void getValues(const std::shared_ptr<bmcweb::AsyncResp>& aResp,
         .jsonValue["Oem"]["IBM"]["InputPowerHistoryItems"]["@odata.type"] =
         "#OemPowerSupplyMetric.InputPowerHistoryItems";
 
-    // const std::array<const char*, 2> interfaces = {
-    //    "org.open_power.Sensor.Aggregation.History.Average",
-    //    "org.open_power.Sensor.Aggregation.History.Maximum"};
-    // const std::array<const char*, inputHistoryItem.size()> interfaces =
-    //    std::to_array(inputHistoryItem);
-    std::array<const char*, inputHistoryItem.size()> interfaces;
-    std::copy(inputHistoryItem.begin(), inputHistoryItem.end(), interfaces);
+    const std::array<const char*, 2> interfaces = {inputHistoryItem[0].c_str(),
+                                                   inputHistoryItem[1].c_str()};
 
     crow::connections::systemBus->async_method_call(
         [aResp, chassisID, powerSupplyID](
